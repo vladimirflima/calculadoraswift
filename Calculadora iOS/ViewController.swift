2f40e61b4d829145f7e3aa27f2eb925a9ac81ef3
Vladimir Lima <vladimirflima@outlook.com>
2024-07-23 12:04:08 -0300

    integração View Controller com Representable

--- Calculadora iOS/ViewController.swift
+++ Calculadora iOS/ViewController.swift
@@ -1,55 +1,214 @@
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet weak var displayLabel: UILabel!
+class ViewController: UIViewController
+{
 
-    var currentNumber: Double = 0
-    var previousNumber: Double = 0
-    var performingMath = false
-    var operation = 0
-
-    @IBAction func numbers(_ sender: UIButton) {
-        if performingMath {
-            displayLabel.text = String(sender.tag)
-            currentNumber = Double(displayLabel.text!)!
-            performingMath = false
-        } else {
-            displayLabel.text = displayLabel.text! + String(sender.tag)
-            currentNumber = Double(displayLabel.text!)!
-        }
+    @IBOutlet weak var calculatorWorkings: UILabel!
+    @IBOutlet weak var calculatorResults: UILabel!
+    
+    var workings:String = ""
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        clearAll()
+    }
+    
+    func clearAll()
+    {
+        workings = ""
+        calculatorWorkings.text = ""
+        calculatorResults.text = ""
     }
 
-    @IBAction func buttons(_ sender: UIButton) {
-        if displayLabel.text != "" && sender.tag != 11 && sender.tag != 16 {
-            previousNumber = Double(displayLabel.text!)!
-
-            if sender.tag == 12 { // Divide
-                displayLabel.text = "/";
-            } else if sender.tag == 13 { // Multiply
-                displayLabel.text = "x";
-            } else if sender.tag == 14 { // Subtract
-                displayLabel.text = "-";
-            } else if sender.tag == 15 { // Add
-                displayLabel.text = "+";
-            }
-
-            operation = sender.tag
-            performingMath = true;
-        } else if sender.tag == 16 {
-            if operation == 12 { // Divide
-                displayLabel.text = String(previousNumber / currentNumber)
-            } else if operation == 13 { // Multiply
-                displayLabel.text = String(previousNumber * currentNumber)
-            } else if operation == 14 { // Subtract
-                displayLabel.text = String(previousNumber - currentNumber)
-            } else if operation == 15 { // Add
-                displayLabel.text = String(previousNumber + currentNumber)
-            }
-        } else if sender.tag == 11 {
-            displayLabel.text = ""
-            previousNumber = 0
-            currentNumber = 0
-            operation = 0
+    @IBAction func equalsTap(_ sender: Any)
+    {
+        if(validInput())
+        {
+            let checkedWorkingsForPercent = workings.replacingOccurrences(of: "%", with: "*0.01")
+            let expression = NSExpression(format: checkedWorkingsForPercent)
+            let result = expression.expressionValue(with: nil, context: nil) as! Double
+            let resultString = formatResult(result: result)
+            calculatorResults.text = resultString
         }
+        else
+        {
+            let alert = UIAlertController(
+                title: "Invalid Input",
+                message: "Calculator unable to do math based on input",
+                preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .default))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func validInput() ->Bool
+    {
+        var count = 0
+        var funcCharIndexes = [Int]()
+        
+        for char in workings
+        {
+            if(specialCharacter(char: char))
+            {
+                funcCharIndexes.append(count)
+            }
+            count += 1
+        }
+        
+        var previous: Int = -1
+        
+        for index in funcCharIndexes
+        {
+            if(index == 0)
+            {
+                return false
+            }
+            
+            if(index == workings.count - 1)
+            {
+                return false
+            }
+            
+            if (previous != -1)
+            {
+                if(index - previous == 1)
+                {
+                    return false
+                }
+            }
+            previous = index
+        }
+        
+        return true
+    }
+    
+    func specialCharacter (char: Character) -> Bool
+    {
+        if(char == "*")
+        {
+            return true
+        }
+        if(char == "/")
+        {
+            return true
+        }
+        if(char == "+")
+        {
+            return true
+        }
+        return false
+    }
+    
+    func formatResult(result: Double) -> String
+    {
+        if(result.truncatingRemainder(dividingBy: 1) == 0)
+        {
+            return String(format: "%.0f", result)
+        }
+        else
+        {
+            return String(format: "%.2f", result)
+        }
+    }
+    
+    @IBAction func allClearTap(_ sender: Any)
+    {
+        clearAll()
+    }
+    
+    @IBAction func backTap(_ sender: Any)
+    {
+        if(!workings.isEmpty)
+        {
+            workings.removeLast()
+            calculatorWorkings.text = workings
+        }
+    }
+    
+    func addToWorkings(value: String)
+    {
+        workings = workings + value
+        calculatorWorkings.text = workings
+    }
+    
+    @IBAction func percentTap(_ sender: Any)
+    {
+        addToWorkings(value: "%")
+    }
+    
+    @IBAction func divideTap(_ sender: Any)
+    {
+        addToWorkings(value: "/")
+    }
+    
+    @IBAction func timesTap(_ sender: Any)
+    {
+        addToWorkings(value: "*")
+    }
+    
+    @IBAction func minusTap(_ sender: Any)
+    {
+        addToWorkings(value: "-")
+    }
+    
+    @IBAction func plusTap(_ sender: Any)
+    {
+        addToWorkings(value: "+")
+    }
+    
+    @IBAction func decimalTap(_ sender: Any)
+    {
+        addToWorkings(value: ".")
+    }
+    
+    @IBAction func zeroTap(_ sender: Any)
+    {
+        addToWorkings(value: "0")
+    }
+    
+    @IBAction func oneTap(_ sender: Any)
+    {
+        addToWorkings(value: "1")
+    }
+    
+    @IBAction func twoTap(_ sender: Any)
+    {
+        addToWorkings(value: "2")
+    }
+    
+    @IBAction func threeTap(_ sender: Any)
+    {
+        addToWorkings(value: "3")
+    }
+    
+    @IBAction func fourTap(_ sender: Any)
+    {
+        addToWorkings(value: "4")
+    }
+    
+    @IBAction func fiveTap(_ sender: Any)
+    {
+        addToWorkings(value: "5")
+    }
+    
+    @IBAction func sixTap(_ sender: Any)
+    {
+        addToWorkings(value: "6")
+    }
+    
+    @IBAction func sevenTap(_ sender: Any)
+    {
+        addToWorkings(value: "7")
+    }
+    
+    @IBAction func eightTap(_ sender: Any)
+    {
+        addToWorkings(value: "8")
+    }
+    
+    @IBAction func nineTap(_ sender: Any)
+    {
+        addToWorkings(value: "9")
     }
 }
--- Calculadora iOS/ViewControllerWrapper.swift
+++ Calculadora iOS/ViewControllerWrapper.swift
@@ -1,25 +1,33 @@
 //
-//  ContentView.swift
+//  ViewControllerWrapper.swift
 //  Calculadora iOS
 //
-//  Created by Vladimir Lima on 08/07/24.
+//  Created by Vladimir Lima on 23/07/24.
 //
 
 import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        ViewControllerWrapper()
-            .edgesIgnoringSafeArea(.all)
-    }
-}
+import UIKit
 
 struct ViewControllerWrapper: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> ViewController {
         return ViewController()
     }
-
+    
     func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+    }
+    
+    class Coordinator: NSObject {
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator()
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ViewControllerWrapper()
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
